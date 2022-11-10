9741e540693a19369de0c0c3460afa1e5338a15c
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-11-10 13:39:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main()
   cin >> a >> b >> c;
 
   //Qui va scritto il codice per eseguire i calcoli richiesti
-    cout << (a*b/2) << endl;
+    cout << ((a*b)/2) << endl;
     cout << (a*a) << endl;
     cout << (a*b) << endl;
-    cout << ((a+b)*c/2) << endl;
+    cout << (((a+b)*c)/2);
 }
